b337820250a03589e9789df18b3bc9b9237c0e03
gillchristian <gillchristiang@gmail.com>
2017-07-31 14:52:52 -0300

    Update SW to return cached assets

--- sw.js
+++ sw.js
@@ -5,9 +5,10 @@ console.log('WORKER: executing.')
 const VERSION = 'v1'
 
 const offlineAssets = [
-  '',
-  'index.js',
-  'index.css',
+  './',
+  './index.html',
+  './index.js',
+  './index.css',
 ]
 
 self.addEventListener('install', (event) => {
@@ -22,6 +23,29 @@ self.addEventListener('install', (event) => {
     })
   )
 
+})
+
+self.addEventListener('fetch', (event) => {
+  console.log('WORKER: fetch event in progress.')
+
+  if (event.request.method !== 'GET') {
+    console.log(`WORKER: fetch event ignored: ${event.request.method} ${event.request.url}`)
+    return
+  }
+
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((cached) => {
+        const networked = fetch(event.request)
+          .then(fetchedFromNetwork(caches))
+          .catch(unableToResolve)
+
+        console.log(`WORKER: fetch event: ${cached ? '(cached)' : '(network)'} ${event.request.url}`)
+
+        return cached || networked
+      })
+  )
 })
 
 self.addEventListener('activate', (event) => {
@@ -40,3 +64,33 @@ self.addEventListener('activate', (event) => {
       })
   )
 })
+
+// ----- HELPERS -----
+function unableToResolve () {
+  console.log('WORKER: fetch request failed in both cache and network.')
+
+  return new Response('<h1>Service Unavailable</h1>', {
+    status: 503,
+    statusText: 'Service Unavailable',
+    headers: new Headers({
+      'Content-Type': 'text/html'
+    })
+  })
+}
+
+function fetchedFromNetwork(caches) {
+  return (response) => {
+    const cacheCopy = response.clone()
+
+    console.log(`WORKER: fetch response from network: ${event.request.url}`)
+
+    caches
+      .open(`${VERSION}-pages`)
+      .then((cache) => cache.put(event.request, cacheCopy))
+      .then(() => {
+        console.log(`WORKER: fetch response stored in cache: ${event.request.url}`)
+      })
+
+    return response
+  }
+}
